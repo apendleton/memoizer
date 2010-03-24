987a46d8f11b243e024cb22fe54fff32c131d0af
Thomas Low <code@thomaslow.com>
2010-03-24 19:39:53 +0100

    Add some comments to the example files

--- examples/fibonacci.py
+++ examples/fibonacci.py
@@ -1,26 +1,32 @@
+"""Fibonacci example demonstrating speed-up
+
+This example demostrates the speed-up a program can gain when using function
+memoization.
+
+Note: Of course fibonacci numbers can be computed way more efficiently.
+"""
 
 import time
 from memoizer import memoize
 
 @memoize()
-def fib(n):
+def fib(n): #cached fibonacci function
 	return (n<=2) and 1 or fib(n-1)+fib(n-2)
 
-def fib_old(n):
+def fib_old(n): #uncached fibonacci function
 	return (n<=2) and 1 or fib_old(n-1)+fib_old(n-2)
 
 if __name__ == "__main__":
 	print "	fib		memoized fib"
-	for n in [1,5,10,20,30,35]:
+	for n in [1,5,10,20,30,35]: #test speed difference for some numbers
 		print "n=%d	" % n,
 		
 		#test regular fibonacci
 		started = time.time()
-		fib_old(n)	
+		fib_old(n)
 		print "%.5f		" % (time.time()-started),
 		
 		#test cached fibonacci
 		started  = time.time()
-		fib(n)		
+		fib(n)
 		print "%.5f" % (time.time()-started)
-	
--- examples/line_counter.py
+++ examples/line_counter.py
@@ -1,26 +1,36 @@
+"""Example demonstrating persistent caching.
+
+This example shows how you can decorate a function in a way that its result is
+stored persistently, yet the cache recognises when the function has to be 
+re-evaluated."""
 
 import time, os.path
 from memoizer import memoize
 
-
 class LineCounter(object):
+	"""Counts the number of lines of a file"""
 	
 	def __init__(self, path):
 		self.path = path
-		self.f = open(path, "r")
 	
 	@memoize(backend='pickle')
 	def count(self):
-		time.sleep(1)
-		return len(self.f.readlines())
+		"""Is executed at most once, then the cached result is returned."""
+		time.sleep(1) #makes sure you can see the difference
+		f = open(self.path, "r")
+		lines = len(f.readlines())
+		f.close()
+		return lines		
 	
 	def __hash__(self):
-		print os.path.getmtime(self.path)
+		"""Computes a unique hash that only changes when 'count' needs to be 
+		evaluated again."""
 		return hash((
 			self.path,
 			os.path.getmtime(self.path)
 		))
-	
+
+
 if __name__ == "__main__":
 	lc = LineCounter(__file__)
 	#first
@@ -33,6 +43,5 @@ if __name__ == "__main__":
 	count = lc.count()
 	print "Second: counted %d lines in %.5f" % (count,time.time()-started)
 	
+	#make sure cache is stored persisently
 	LineCounter.count.sync()
-	
-	
